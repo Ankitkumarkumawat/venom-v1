b03dad3f43f09e29d25ab3ee420e5283ca8d2a54
Ankitkumarkumawat <ankitkumawat581@gmail.com>
2024-08-26 23:48:38 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7513431675:AAEhsBWDQ8xF4-ZREM0i2nMk0ZtZ7Hseq-k'
-ADMIN_ID = '1596078576'
+API_TOKEN = '7235894377:AAG58upyNQkS4xvDB4_gly3_JljmIesROJw'
+ADMIN_ID = '1492749701'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
